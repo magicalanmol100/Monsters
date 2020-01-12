5f9e672a24a18840182f76b2baa173b3ea450f59
unknown <magicalanmol100@gmail.com>
2020-01-12 11:24:48 +0530

    Updates

---
+++ precache-manifest.62019bdf74e07f2570b207b00c868a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b6f856e023191e0101c14aff9726c2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5de5012a151630fefd35",
+    "url": "/monsters/static/css/main.2c871bb3.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monsters/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5de5012a151630fefd35",
+    "url": "/monsters/static/js/main.e4caaa44.chunk.js"
+  },
+  {
+    "revision": "0f2e201d39817ce23cac",
+    "url": "/monsters/static/js/runtime-main.b4ddb502.js"
+  }
+]);
